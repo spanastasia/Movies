b9cfbdcabd813cd4161efb7eecabe8a9b69178d9
Anastasiia Spiridonova <anastasiia_spiridonova@epam.com>
2023-06-16 16:09:39 +0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Anastasiia Spiridonova on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
